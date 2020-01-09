891bcfbc49d50bc05861d0ee58cb6e68d5488dd2
Zahed Hossain <32036354+zahedbri@users.noreply.github.com>
2020-01-09 16:51:18 +0600

    Chnages
    Nuxt files

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# service-portfolio
-A service based portfolio app
+# Zack Proser's portfolio site
+
+[![Netlify Status](https://api.netlify.com/api/v1/badges/829157fb-72fc-40e9-b710-db7070ed8f7d/deploy-status)](https://app.netlify.com/sites/gifted-albattani-6bea85/deploys)
+
+> Vue Argon Design System for Nuxt
+
+## Build Setup
+
+``` bash
+# install dependencies
+$ npm install
+
+# serve with hot reload at localhost:3000
+$ npm run dev
+
+# build for production and launch server
+$ npm run build
+$ npm start
+
+# generate static project
+$ npm run generate
+```
+
+For detailed explanation on how things work, checkout [Nuxt.js docs](https://nuxtjs.org).
---
+++ nuxt.config.js
@@ -0,0 +1,128 @@
+const pkg = require('./package')
+
+const glob = require('glob')
+const path = require('path')
+
+var getDynamicRoutes = function() {
+  return [].concat(
+    glob
+      .sync('*.md', { cwd: 'posts/' })
+      .map((filepath) => `/software/${path.basename(filepath, '.md')}`),
+    glob
+      .sync('*.md', { cwd: 'blog/' })
+      .map((filepath) => `/blog/${path.basename(filepath, '.md')}`)
+  )
+}
+
+var dynamicPaths = getDynamicRoutes()
+
+module.exports = {
+  generate: {
+    routes: dynamicPaths
+  },
+  mode: 'universal',
+
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: pkg.name,
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'X-UA-Compatible', content: 'IE=edge' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1.0, maximum-scale=1.5, user-scalable=1, shrink-to-fit=no' },
+      { hid: 'description', name: 'description', content: pkg.description },
+      { name: 'author', content: 'Zack Proser' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700' }
+    ]
+  },
+
+  /*
+  ** Configuration for @nuxtjs/pwa
+  ** https://developer.mozilla.org/en-US/docs/Web/Manifest
+  */
+  manifest: {
+    name: 'Zack Proser Portfolio',
+    short_name: 'Zack Proser',
+    description: 'Software engineer and artist',
+    theme_color: '#172b4d',
+  },
+
+  meta: {
+    // apple-mobile-web-app-capable=yes
+    // https://medium.com/@firt/dont-use-ios-web-app-meta-tag-irresponsibly-in-your-progressive-web-apps-85d70f4438cb
+    mobileAppIOS: true,
+    appleStatusBarStyle: '#172b4d'
+  },
+
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+
+  /*
+  ** Global CSS
+  */
+  css: [
+    '~assets/argon/vendor/nucleo/css/nucleo.css',
+    '@fortawesome/fontawesome-free/css/all.css',
+    '~assets/argon/scss/argon.scss',
+    'bootstrap-vue/dist/bootstrap-vue.css',
+    '~assets/transitions.css'
+  ],
+
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+    '~/plugins/argon/argon-kit',
+    '~/plugins/common.js'
+  ],
+
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    // Doc: https://axios.nuxtjs.org/usage
+    '@nuxtjs/axios',
+    // Doc: https://bootstrap-vue.js.org/docs/
+    ['bootstrap-vue/nuxt', {
+      bootstrapCSS: false,
+      bootstrapVueCSS: false,
+      componentPlugins: [
+        'Carousel',
+        'Spinner'
+      ],
+      directivePlugins: [
+        'Tooltip',
+        'Popover'
+      ]
+    }],
+    '@nuxtjs/pwa'
+  ],
+  /*
+  ** Axios module configuration
+  */
+  axios: {
+    // See https://github.com/nuxt-community/axios-module#options
+  },
+
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+      config.module.rules.push({
+        test: /\.md$/,
+        include: [path.resolve(__dirname, 'posts'), path.resolve(__dirname, 'blog')],
+        loader: 'frontmatter-markdown-loader'
+      })
+    }
+  }
+}
